c6b659040c0f4a87c10288c9f478f8ffa1c787cd
andizaid <andizaid27@gmail.com>
2025-12-09 07:12:35 +0800

    make routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\FeedbackController;
+use App\Http\Controllers\ScreeningController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::get('/screening', [ScreeningController::class, 'create'])->name('screening.create');
+Route::post('/screening', [ScreeningController::class, 'sotre'])->name('screening.store');
+Route::get('/screening/{screening}', [ScreeningController::class, 'result'])->name('screening.result');
+
+Route::post('/feedback', [FeedbackController::class, 'store'])->name('feedback.store');

601ec3bd5aa16d2a653ceb6189016428b8521b25
andizaid <andizaid27@gmail.com>
2025-12-09 06:45:30 +0800

    make models

--- app/Models/Respondent.php
+++ app/Models/Respondent.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Respondent extends Model
 {
-    //
+    protected $fillable = [
+        'name', 'gender', 'age', 'occupation', 'education', 'weight', 'height'
+    ];
+
+    public function screenings()
+    {
+        return $this->hasOne(Screening::class);
+    }
 }
--- app/Models/Screening.php
+++ app/Models/Screening.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Screening extends Model
 {
-    //
+    protected $fillable = [];
+
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+
+        $this->fillable = array_merge(
+            ['respondent_id'],
+            array_map(fn($i) => "q$i", range(1,15)),
+            ['total_score','kategori']
+        );
+    }
+
+    public function respondent()
+    {
+        return $this->belongsTo(Respondent::class);
+    }
 }
